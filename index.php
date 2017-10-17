622b69f7dd986a45d2c3006c6f91c76e767274ed
christian99salerno <christian.salerno05@gmail.com>
2017-10-17 20:46:59 +0200

    database connection

--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 
   <head>
     <title>Site</title>
-<link href="style.css" rel="stylesheet">  
+<link href="style.css" rel="stylesheet">
 </head>
 
+
+<?php
+
+$connessione = mysqli_connect(localhost,root,root);
+$selezione = mysqli_select_db(Games,$connessione);
+
+if(!$connessione) {
+   echo "Connection failed";
+}
+
+echo "Connected successfully";
+
+$query = mysqli_query("SELECT * FROM list_games_2017");
+
+?>
 
 <body>
 
@@ -22,7 +37,11 @@
 
 <div id="divisione1"></div>
 
-<div class="divisioni"></div>
+<?php
+
+$dato = mysqli_fetch_assoc()
+
+?>
 
 <div class="divisioni"></div>
 
@@ -30,8 +49,15 @@
 
 <div class="divisioni"></div>
 
-<div class="divisioni"></div>	
+<div class="divisioni"></div>
 
+<div class="divisioni"></div>
+
+<?php
+
+mysqli_close($connessione);
+
+?>
 
 </body>
 

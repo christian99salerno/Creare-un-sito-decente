74be9f45b6d95187605df4b045c87d9b69793dda
christian99salerno <christian.salerno05@gmail.com>
2017-11-25 08:18:58 +0100

    rivoluzione estetica

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <html>
 
 <head>
-    <title>Site</title>
-<link href="style.css" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<title>Site</title>
+
+<link rel="stylesheet" href="bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+<script src="jquery-3.2.1.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="bootstrap.min.js"></script>
+
 </head>
 
 <?php
@@ -21,54 +27,92 @@ $result = mysqli_query($connessione,$myquery);
 
 <body>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Forever Gamers</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="staff.php">Staff</a>
+      </li>
+      <li class="nav-item">
+<a class="nav-link" href="community.php">Community</a>
+      </li>
+      <li class="nav-item">
+<a class="nav-link" href="battaglia_navale.php">Battaglia Navale</a>
+      </li>
+      <li class="nav-item">
+<a class="nav-link" href="live.php">Live</a>
+      </li>
+      
+      
+    </ul>
+  </div>
+</nav>
 
-<div style="width:100%; height:5%; text-align:center; font-size:20px; margin-top:20px">Benvenuto</div>
+<div class="container">
 
-<div class="pulsante"><a href="home.php">Home</a></div>
+<div class="row justify-content-center">
+<div class="col-6">
+<div class="jumbotron">
+  <h4 class="text-center">Entra a far parte della nostra Community</h4>
+</div>
+</div>
+</div>
 
-<div class="pulsante"><a href="staff.php">Staff</a></div>
-
-<div class="pulsante"><a href="community.php">Community</a></div>
-
-<div class="pulsante"><a href="battaglia_navale.php">Battaglia Navale</a></div>
-
-<div class="pulsante"><a href="live.php">Live</a></div>
-
-<div style="width:50%; height:10%; margin-top:100px; font-size:20px">Entra a far parte della nostra Community</div>
-
+<div class="row justify-content-center">
+<div class="col-6">
 <form action="memorizza_dati.php" method="post">
 
-<input placeholder="Nome" name="nome">
+<div class="form-group">
+ 
+ <label for="nome">Nome</label>
+ <input type="text" class="form-control" id="nome" placeholder="Nome" name="nome">
 
-</br></br>
+</div>
 
-<input placeholder="Cognome" name="cognome">
+<div class="form-group">
+ 
+ <label for="cognome">Cognome</label>
+ <input type="text" class="form-control" id="cognome" placeholder="Cognome" name="cognome">
 
-</br></br>
+</div>
 
-<input placeholder="Username" name="username">
+<div class="form-group">
+ 
+ <label for="username">Username</label>
+ <input type="text" class="form-control" id="username" placeholder="Username" name="username">
 
-</br></br>
+</div>
 
-<input placeholder="Password" name="password">
+<div class="form-group">
 
-</br></br>
+<label for="password">Password</label>
+<input type="password" class="form-control" id="password" placeholder="Password" name="password">
+
+</div>
 
 Male<input type="radio" name="sesso" value="maschio">
 Female<input type="radio" name="sesso" value="femmina">
 
-</br></br>
-
-<input type="submit" nome="registrati" value="Registrati">
+<input type="submit" class="btn btn-primary btn-block" nome="registrati" value="Registrati">
 
 </form>
-
+</div>
+</div>
 <?php
 
 mysqli_close($connessione);
 
 ?>
+
+</div>
 
 </body>
 

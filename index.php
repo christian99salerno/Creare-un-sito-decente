c0735728aa7767213cac3abf62ee7ac8a04c464a
christian99salerno <christian.salerno05@gmail.com>
2017-11-28 17:52:49 +0100

    modifiche irrilevanti

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <title>Site</title>
 
-<link rel="stylesheet" href="bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<link rel="stylesheet" href="bootstrap.min.css">
 
 <script src="jquery-3.2.1.js"></script>
 
